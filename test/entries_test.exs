8bc9e37b4867b32fe72ef66a186c088c38ee5c98
Christophe De Troyer <christophe.detroyer@gmail.com>
2021-05-22 13:13:25 +0200

    entry works

---
+++ test/entries_test.exs
@@ -0,0 +1,29 @@
+defmodule BibtexParser.Test.Entries do
+  use ExUnit.Case
+  doctest BibtexParser
+
+  test "Simple Entries" do
+    input = "@book{foo, year = 1234} @notabook{foo, year = 1234}"
+
+    {:ok, ast, _, _, _, _} = BibtexParser.AST.entry(input)
+
+    expected = [
+      %AST.Entry{
+        entry_type: "book",
+        fields: [
+          %AST.Field{key: %AST.Key{content: "year"}, value: [%AST.Number{content: 1234}]}
+        ],
+        internal_key: "foo"
+      },
+      %AST.Entry{
+        entry_type: "notabook",
+        fields: [
+          %AST.Field{key: %AST.Key{content: "year"}, value: [%AST.Number{content: 1234}]}
+        ],
+        internal_key: "foo"
+      }
+    ]
+
+    assert expected == ast
+  end
+end
