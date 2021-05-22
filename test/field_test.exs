2e586a50084380ed3860b2b4866402181b99b68f
Christophe De Troyer <christophe.detroyer@gmail.com>
2021-05-22 13:04:08 +0200

    multiple fields

--- test/field_test.exs
+++ test/field_test.exs
@@ -29,8 +29,21 @@ defmodule BibtexParser.Test.Field do
     assert expected == ast
   end
 
-  test "Multiple fields separated by comma, and optional trailing comma" do
+  test "Multiple fields separated by comma" do
     input = ~s(year=1999,year=2000)
+
+    {:ok, ast, _, _, _, _} = BibtexParser.AST.fields(input)
+
+    expected = [
+      %AST.Field{key: %AST.Key{content: "year"}, value: [%AST.Number{content: 1999}]},
+      %AST.Field{key: %AST.Key{content: "year"}, value: [%AST.Number{content: 2000}]}
+    ]
+
+    assert expected == ast
+  end
+
+  test "Multiple fields separated by comma, and trailing comma" do
+    input = ~s(year=1999,year=2000,)
 
     {:ok, ast, _, _, _, _} = BibtexParser.AST.fields(input)
 
