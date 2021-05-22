e49dc458e801d5f15d2d0d559ace058fbf49e186
Christophe De Troyer <christophe.detroyer@gmail.com>
2021-05-22 13:10:35 +0200

    simple entries work

--- test/entry_test.exs
+++ test/entry_test.exs
@@ -2,8 +2,26 @@ defmodule BibtexParser.Test.Entry do
   use ExUnit.Case
   doctest BibtexParser
 
-  test "Entry" do
+  test "Simple Entry" do
     input = "@book{foo, year = 1234}"
+
+    {:ok, ast, _, _, _, _} = BibtexParser.AST.entry(input)
+
+    expected = [
+      %AST.Entry{
+        entry_type: "book",
+        fields: [
+          %AST.Field{key: %AST.Key{content: "year"}, value: [%AST.Number{content: 1234}]}
+        ],
+        internal_key: "foo"
+      }
+    ]
+
+    assert expected == ast
+  end
+
+  test "Entry with braced fields" do
+    input = "@book{foo, year = {1234}}"
 
     {:ok, ast, _, _, _, _} = BibtexParser.AST.entry(input)
 
